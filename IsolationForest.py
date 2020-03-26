3eaab8983a27dd2957a4d855384fccda2645a147
Kaushal Kishore <kshr4kshl@gmail.com>
2020-03-26 10:57:26 +0530

    handling the dataframe object input

--- IsolationForest.py
+++ IsolationForest.py
@@ -1,4 +1,6 @@
+from copy import deepcopy
 import IsolationTree
+import pandas as pd
 
 class IsolationForest(object):
     """
@@ -14,6 +16,11 @@ class IsolationForest(object):
 
     copy_x : bool, optional, default True
         If True, dataset x will be copied; else, it may be overwritten.
+
+    df : array-like of shape (n_samples, n_features)
+        Training data
+
+
     """
 
     def __init__(self, num_trees: int = 128, subsample_size: int = 256,
@@ -21,7 +28,21 @@ class IsolationForest(object):
         self.num_trees = num_trees
         self.subsample_size = subsample_size
         self.copy_x = copy_x
+        self.df = None
 
-    def fit(self, ):
-        """ Fit the  """
-        pass
+    def fit(self, df):
+        """
+        Fit the Isolation Forest model on the dataset
+
+        Parameters
+        ----------
+        df : pd.DataFrame object
+        """
+        if self.copy_x:
+            self.df = deepcopy(df)
+        else:
+            self.df = df
+
+        if not isinstance(self.df, pd.DataFrame):
+            self.df = pd.DataFrame(data=self.df)
+
