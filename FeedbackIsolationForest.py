204cdec09882b4f67dd9e2a35685beb32164580a
Kaushal Kishore <kshr4kshl@gmail.com>
2020-03-29 22:07:22 +0530

    Documentation

--- FeedbackIsolationForest.py
+++ FeedbackIsolationForest.py
@@ -9,27 +9,34 @@ class FeedbackIsolationForest(IsolationForest):
         Parameters
         ----------
         num_trees : int, optional, default 128
-            Number of isolation trees in the forest.
+            Number of isolation trees in the forest. Inherited.
 
         subsample_size : int, optional, default 256
-            Sampling size for the dataset for isolation trees.
+            Sampling size for the dataset for isolation trees. Inherited.
 
         copy_x : bool, optional, default True
-            If True, dataset x will be copied; else, it may be overwritten.
+            If True, dataset x will be copied; else, it may be overwritten. Inherited.
+
+        loss_fn: str, optional, default linear
+            Loss function to use with mirror descent algorithm.
+
+        set_member: function-type, default returns True for all values
+            A function which is used to check set membership of the weights in the desired set.
+            For a tree, it should be (lambda wt => (wt >= 0)).
 
         Attributes
         ----------
 
         isolation_trees : list, default None
-            List of IsolationTree
+            List of IsolationTree. Inherited.
 
         df : array-like of shape (n_samples, n_features)
-            Training data.
+            Training data. Inherited.
 
         """
 
     def __init__(self, num_trees: int = 128, subsample_size: int = 256, copy_x: bool = True,
-                 loss_fn : str = 'linear', set_member = (lambda t: True)):
+                 loss_fn : str = 'linear', set_member = (lambda wt: (wt >= 0))):
 
         if loss_fn not in LOSS_FN_TYPES:
             raise ValueError('{} loss not supported'.format(loss_fn))
