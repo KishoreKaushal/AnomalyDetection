0c558c4a260e0f46ce2bffff495bcfbe9bba12b6
Kaushal Kishore <kshr4kshl@gmail.com>
2020-03-31 12:01:30 +0530

    removing inheritance, adding function update weights

--- FeedbackIsolationForest.py
+++ FeedbackIsolationForest.py
@@ -1,8 +1,8 @@
-import IsolationForest
+import FeedbackIsolationTree
 
 LOSS_FN_TYPES = frozenset(['linear', 'log-likelihood'])
 
-class FeedbackIsolationForest(IsolationForest):
+class FeedbackIsolationForest(object):
     """
         Feedback Guided Isolation Forest
 
@@ -44,15 +44,39 @@ class FeedbackIsolationForest(IsolationForest):
         if loss_fn not in LOSS_FN_TYPES:
             raise ValueError('{} loss not supported'.format(loss_fn))
 
-        super(FeedbackIsolationForest, self).__init__(num_trees, subsample_size, copy_x)
+        self.num_trees = num_trees
+        self.subsample_size = subsample_size
+        self.copy_x = copy_x
+        self.df = None
+        self.feedback_isolation_trees = None
+
         self.loss_fn = loss_fn
         self.set_member = set_member
         self.lrate = lrate
 
+    def update_weights(self, hlim, feedback, lrate, inst):
+        """
+        Parameters:
+        -----------
+        hlim : int
+            Height (or depth) limit of the tree.
 
-    def update_weights(self):
+        inst : pd.DataFrame
+            An instance which is used to compute loss.
 
-        pass
+        lrate : numeric value
+            Learning rate of the mirror descent algorithm.
 
-    def score(self):
+        feedback : {+1, -1}
+            +1 means that it is the inst is anomaly otherwise not.
+        """
+
+        # this can be parallelized using -- map function
+        for ftree in self.feedback_isolation_trees:
+            ftree.update_weights(hlim, feedback, lrate, inst)
+
+        return self
+
+    def score(self, df_inst):
+        
         pass
