3667dc8fb89f535e584e6e8c6575c21ec28f3df2
Kaushal Kishore <kshr4kshl@gmail.com>
2020-04-03 12:36:47 +0530

    adding tests

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import argparse
 
 
 def main():
+
     parser = argparse.ArgumentParser(add_help=True, description='testing feedback guided isolation forest model')
-    parser.add_argument('-n', '--ntrees', type=int, default=100, help='number of trees in the forest, default 100')
+    parser.add_argument('-n', '--ntrees', type=int, default=128, help='number of trees in the forest, default 100')
     parser.add_argument('-s', '--samplesize', type=int, default=256, help='sampling rate for each tree, default 256')
     # parser.add_argument('-l', '--loss', type=str, default='linear', help='loss function linear or log-likelihood, default linear')
     parser.add_argument('-lr', '--lrate', type=float, help='learning rate of mirror descent algorithm')
@@ -15,6 +16,12 @@ def main():
     required_named.add_argument('-o', '--output', type=str, help='path for results file', required=True)
     parser.parse_args()
 
+
+def test_isolation_forest(df, ntrees, samplesize, hlim):
+    pass
+
+def test_feedback_isolation_forest(df, ntrees, samplesize, hlim, lrate):
+    pass
 
 if __name__ == '__main__':
     main()
