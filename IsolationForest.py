0fffd96b8a84030f3edbe57ddce5d13229d4c7e8
Kaushal Kishore <kshr4kshl@gmail.com>
2020-04-04 10:36:33 +0530

    add args to path length and fixed reshape bug

--- IsolationForest.py
+++ IsolationForest.py
@@ -65,7 +65,7 @@ class IsolationForest(object):
         subsamples = sample(range(num_inst), self.subsample_size * self.num_trees)
 
         # partitioning into self.num_trees equal chunks of size self.subsample_size
-        subsamples = np.array(subsamples).reshape(shape=(self.num_trees, self.subsample_size))
+        subsamples = np.array(subsamples).reshape((self.num_trees, self.subsample_size))
 
         # creating isolation trees -- this code can be parallelized -- using map function
         self.isolation_trees = [IsolationTree().fit(df.loc[subsample])
@@ -73,13 +73,16 @@ class IsolationForest(object):
 
         return self
 
-    def anomaly_score(self, df_inst):
+    def anomaly_score(self, df_inst, hlim):
         if not isinstance(df_inst, pd.DataFrame):
             df_inst = pd.DataFrame(data=df_inst)
 
+        num_inst, _ = df_inst.shape
+
         # this can be parallelized -- using map function
         # scores here is a 2D array of shape => (num_trees, num_df_inst)
-        path_lengths = np.array([i_tree.path_length(df_inst) for i_tree in self.isolation_trees])
+        path_lengths = np.array([i_tree.path_length(df_inst, hlim, num_inst)
+                                 for i_tree in self.isolation_trees])
 
         # avg_path_lengths store average path length of an instance in df_inst
         # shape of avg_path_lengths => (num_df_inst,)
