720abc0981e4b417046efa6d4bcca6ced786e2fc
Kaushal Kishore <kshr4kshl@gmail.com>
2020-04-06 19:30:27 +0530

    testing isolation forest

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from IsolationForest import IsolationForest
 from FeedbackIsolationForest import FeedbackIsolationForest
+import utils
+from copy import deepcopy
 
 """
 Input file must be a pickle file containing dataframe 
@@ -21,6 +23,8 @@ def parse_args():
     parser.add_argument('-s', '--subsamplesize', type=int, default=256, help='sampling rate for each tree, default 256')
     # parser.add_argument('-l', '--loss', type=str, default='linear', help='loss function linear or log-likelihood, default linear')
     parser.add_argument('-lr', '--lrate', type=float, help='learning rate of mirror descent algorithm')
+    parser.add_argument('-ts', '--testsize', type=float, default=0.05, help='test size in ration')
+    parser.add_argument('-et', '--evaltrain', type=bool, default=False, help='whether to find accuracy on training data')
 
     required_named = parser.add_argument_group('required named arguments')
 
@@ -31,6 +35,49 @@ def parse_args():
     return parser.parse_args()
 
 
+def test_feedback_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
+                                   ntrees, samplesize, hlim, lrate):
+    pass
+
+
+def test_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
+                          ntrees, subsample_size, hlim, eval_train):
+    threshold = 0.5
+
+    print("--- Isolation Forest Testing ---")
+    print("Training data shape: ", df_x_train.shape)
+    print("ntrees: {}, subsample_size: {}, hlim: {}"
+          .format(ntrees, subsample_size, hlim))
+
+    iforest = IsolationForest(ntrees, subsample_size, False)
+
+    t0 = time.time()
+    iforest.fit(df_x_train)
+    print("Training finished in : {}".format(time.time() - t0))
+
+    # compute sore
+
+    score = iforest.anomaly_score(df_x_train, hlim)
+
+    df = deepcopy(df_x_train)
+    df['score'] = score
+
+    df.to_pickle('./results/results_hlim_{}.pickle'.format(hlim))
+    # # calculate training accuracy
+    # if eval_train:
+    #     pred = (iforest.anomaly_score(df_x_train, hlim) > threshold).astype(np.uint8)
+    #     train_acc = (df_y_train == pred).sum()[0] / df_y_train.shape[0]
+    #     print("train acc : {}".format(hlim, train_acc))
+    #
+    # if df_y_test.shape[0] <= 20:
+    #     # calculate test accuracy
+    #     t0 = time.time()
+    #     scores = iforest.anomaly_score(df_x_test, hlim)
+    #     pred = (scores > threshold).astype(np.uint8)
+    #     test_acc = (df_y_test == pred).sum() / df_y_test.shape[0]
+    #     print("test time: {}, test acc : {}".format(time.time() - t0, test_acc))
+
+
 def main():
     args = parse_args()
 
@@ -38,7 +85,8 @@ def main():
     df = shuffle(df)
     df.reset_index(inplace=True, drop=True)
 
-    df_train, df_test = train_test_split(df, test_size=0.01)
+
+    df_train, df_test = train_test_split(df, test_size=args.testsize)
     df_train.reset_index(inplace=True, drop=True)
     df_test.reset_index(inplace=True, drop=True)
 
@@ -50,43 +98,11 @@ def main():
     df_x_test, df_y_test = df_test.loc[:, :col], df_test.loc[:, col]
 
     test_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
-                          args.ntrees, args.subsamplesize, args.hlim)
+                          args.ntrees, args.subsamplesize, args.hlim, args.evaltrain)
 
     test_feedback_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
                                    args.ntrees, args.subsamplesize, args.hlim, args.lrate)
 
-
-def test_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
-                          ntrees, subsample_size, hlim):
-
-    threshold = 0.5
-
-    print("--- Isolation Forest Testing ---")
-    print("ntrees: {}, subsample_size: {}, hlim: {}"
-          .format(ntrees, subsample_size, hlim))
-
-    iforest = IsolationForest(ntrees, subsample_size, False)
-
-    t0 = time.time()
-    iforest.fit(df_x_train)
-    print("Training finished in : {}".format(time.time() - t0))
-    
-    # calculate training accuracy
-    # pred = (iforest.anomaly_score(df_x_train, hlim) > threshold).astype(np.uint8)
-    # train_acc = (df_y_train == pred).sum()[0] / df_y_train.shape[0]
-    # print("train acc : {}".format(hlim, train_acc))
-
-    # calculate test accuracy
-    t0 = time.time()
-    scores = iforest.anomaly_score(df_x_test, hlim)
-    pred = (scores > threshold).astype(np.uint8)
-    test_acc = (df_y_test == pred).sum() / df_y_test.shape[0]
-    print("test time: {}, test acc : {}".format(time.time()-t0, test_acc))
-
-
-def test_feedback_isolation_forest(df_x_train, df_y_train, df_x_test, df_y_test,
-                                   ntrees, samplesize, hlim, lrate):
-    pass
 
 if __name__ == '__main__':
     main()
