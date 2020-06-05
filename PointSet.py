f3bd9c4dddc190158b4280eb190d57ccb0ea6d65
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-06 01:56:32 +0530

    finalized code for creating gap
    Gap is used to transform the k-splitting problem to k-histogram problem. Refer appendix of paper on pidforest.

--- PointSet.py
+++ PointSet.py
@@ -19,11 +19,10 @@ class PointSet(object):
             if len(val) <= 1:
                 gap = [0]
             else:
+                # sum of all gap for a col is equal to start[col] - end[col]
                 gap = np.zeros(len(val))
-                # TODO - complete the code for creating gap for histograms
-                #
-                # gap[0] = (val[0] + val[1]) / 2 - self.node.cube.start[axis]
-                # gap[-1] = self.node.cube.end[axis] - (val[-1] + val[-2]) / 2
-                # for i in range(1, len(val) - 1):
-                #     gap[i] = (val[i + 1] - val[i - 1]) / 2
+                gap[0] = (val[0] + val[1]) / 2 - self.node.cube.start[col]
+                gap[-1] = self.node.cube.end[col] - (val[-1] + val[-2]) / 2
+                for i in range(1, len(val) - 1):
+                    gap[i] = (val[i + 1] - val[i - 1]) / 2
             self.gap[col] = gap
