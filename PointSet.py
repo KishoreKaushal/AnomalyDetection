c373b96bb7a86a62f2f4b2dbb93b68b8404c31b5
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-05 20:53:25 +0530

    compatibility with pandas dataframe column

--- PointSet.py
+++ PointSet.py
@@ -1,18 +1,20 @@
 import numpy as np
+from PIDTree import PIDTree
+
 
 class PointSet(object):
 
     def __init__(self, node, df):
         self.node = node
         self.df = df
-        self.val = []
-        self.count = []
-        self.gap = []
+        self.val = {}
+        self.count = {}
+        self.gap = {}
 
         for col in range(self.df.columns):
             val, count = np.unique(self.df[col], return_counts=True)
-            self.val.append(val)
-            self.count.append(count)
+            self.val[col] = val
+            self.count[col] = count
 
             if len(val) <= 1:
                 gap = [0]
@@ -24,4 +26,4 @@ class PointSet(object):
                 # gap[-1] = self.node.cube.end[axis] - (val[-1] + val[-2]) / 2
                 # for i in range(1, len(val) - 1):
                 #     gap[i] = (val[i + 1] - val[i - 1]) / 2
-            self.gap.append(gap)
+            self.gap[col] = gap
