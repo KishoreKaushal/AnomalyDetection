9b0d5bf87b5af0c28e32517eea6d9e5a5ae12a41
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-05 17:18:10 +0530

    initialization of subcube

--- Cube.py
+++ Cube.py
@@ -1,4 +1,22 @@
+from PIDTree import PIDTree
+import numpy as np
+
 class Cube(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, node, start, end):
+        assert isinstance(node, PIDTree)
+
+        self.node = node
+        self.child = []
+        self.start = start
+        self.end = end
+        self.num_attr = len(start)
+        self.split_axis = -1
+        self.split_vals = []
+
+        # calculating the log-volume of the subcube
+        self.vol = 0
+        for i in range(self.num_attr):
+            self.vol += np.log(self.end[i] - self.start[i])
+
+
