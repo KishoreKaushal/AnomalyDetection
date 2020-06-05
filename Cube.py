49e358ef612cd76a183ae1860dbce9b38c9ab366
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-06 02:43:18 +0530

    returning intervals for the child nodes

--- Cube.py
+++ Cube.py
@@ -1,6 +1,7 @@
 from PIDTree import PIDTree
 import numpy as np
 import pandas as pd
+import copy
 
 class Cube(object):
 
@@ -44,14 +45,37 @@ class Cube(object):
         if df.shape[0] == 0:
             return splits
 
-        splits[0] = df[(self.start[self.split_attr] <= df[self.split_attr])
-                          & (df[self.split_attr] < self.split_vals[0])]
+        # start and end of the interval for each split
+        new_start = [copy.deepcopy(self.start) for _ in range(num_child)]
+        new_end = [copy.deepcopy(self.end) for _ in range(num_child)]
 
-        splits[-1] = df[(self.split_vals[-1] <= df[self.split_attr])
-                          & (df[self.split_attr] < self.end[self.split_attr])]
 
-        for k in range(1, num_child - 1):
-            splits[k] = df[(self.split_vals[k-1] <= df[self.split_attr])
-                          & (df[self.split_attr] < self.split_vals[k])]
+        # new interval for first split
+        # new_start[0][self.split_attr] = self.start[self.split_attr]
+        new_end[0][self.split_attr] = self.split_vals[0]
 
-        return splits
+        # new interval for last split
+        new_start[-1][self.split_attr] = self.split_vals[-1]
+        # new_end[-1][self.split_attr] = self.end[self.split_attr]
+
+        # new interval for the ith split: 0 < i < num_child - 1
+        for i in range(1, num_child-1):
+            new_start[i][self.split_attr] = self.split_vals[i-1]
+            new_end[i][self.split_attr] = self.split_vals[i]
+
+        splits = [df[(new_start[i][self.split_attr] <= df[self.split_attr])
+                    & (df[self.split_attr] <= new_end[i][self.split_attr])]
+                    for i in range(num_child)]
+        #
+        #
+        # splits[0] = df[(self.start[self.split_attr] <= df[self.split_attr])
+        #                   & (df[self.split_attr] < self.split_vals[0])]
+        #
+        # splits[-1] = df[(self.split_vals[-1] <= df[self.split_attr])
+        #                   & (df[self.split_attr] < self.end[self.split_attr])]
+        #
+        # for k in range(1, num_child - 1):
+        #     splits[k] = df[(self.split_vals[k-1] <= df[self.split_attr])
+        #                   & (df[self.split_attr] < self.split_vals[k])]
+
+        return (splits, new_start, new_end)
